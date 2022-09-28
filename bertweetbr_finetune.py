4e65c8d447074e8df1e6d12028f865f35ba74fe0
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2022-09-28 19:29:17 -0300

    Update bertweetbr_finetune.py

--- bertweetbr_finetune.py
+++ bertweetbr_finetune.py
@@ -3,7 +3,7 @@ model_checkpoint = 'neuralmind/bert-base-portuguese-cased'
 tokenizer_checkpoint = 'neuralmind/bert-base-portuguese-cased'
 chunk_size = 282
 batch_size = 32
-train_size = 20000
+train_size = 100
 test_size = int(0.1 * train_size)
 learning_rate = 2e-5
 weight_decay = 0.01
@@ -17,14 +17,18 @@ fp16=False
 
 # Funcao para tokenizacao
 def tokenize_function(examples):
+    examples["labels"] = examples["sentiment"]
     result = tokenizer(examples["tweet_text"], truncation=True)
+    #if tokenizer.is_fast:
+        #result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
     return result
+
 
 
 print('\nETAPA - COLETA DE MODELO E TOKENIZADOR\n')    
 # Pega o model
-from transformers import AutoModelForPreTraining
-model = AutoModelForPreTraining.from_pretrained(model_checkpoint)
+from transformers import AutoModelForPreTraining, AutoModelForTokenClassification, AutoModelForSequenceClassification, BertForPreTraining, BertModel, AutoModel
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=3)
 
 # Pega o tokenizer
 from transformers import AutoTokenizer
@@ -33,6 +37,7 @@ tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
 # Pega o Data Collator
 from transformers import DataCollatorWithPadding
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
 
 
 print('\nETAPA - COLETA DATASET RAW\n')
@@ -44,18 +49,23 @@ from datasets import load_dataset, ClassLabel
 raw_datasets = load_dataset('csv', delimiter=';', data_files={'train': ['./kaggle/trainingdatasets/Train3Classes.csv'], 'validation':['./kaggle/testdatasets/Test3classes.csv'], 'test': ['./kaggle/testdatasets/Test3classes.csv']})
 
 print('\nETAPA - MUDA COLUNA SENTIMENT DE INT PARA CLASSLABEL\n')
-#raw_datasets = raw_datasets.class_encode_column("sentiment")
-feat_sentiment = ClassLabel(num_classes=3, names = ['0', '1', '2'], names_file=None)
-raw_datasets = raw_datasets.cast_column("sentiment", feat_sentiment)
+raw_datasets = raw_datasets.class_encode_column("sentiment")
+#feat_sentiment = ClassLabel(num_classes=3, names = ['0', '1', '2'], names_file=None)
+#raw_datasets = raw_datasets.cast_column("sentiment", feat_sentiment)
 
 print('\nETAPA - FEATURES DE RAW_DATASET\n')
 raw_train_dataset = raw_datasets["train"]
 print(raw_train_dataset.features)
 
+print('\nETAPA - DOWNSAMPLE DO DATASET\n')
+
+
+print('\nETAPA - TOKENIZA DATASET\n')
 # Tokenizando datasets
 tokenized_datasets = raw_datasets.map(
     tokenize_function, batched=True, remove_columns=["id", "tweet_date", "query_used"]
 )
+tokenized_datasets = tokenized_datasets.class_encode_column("labels")
 
 print('\nETAPA - FEATURES DE TOKENIZED_DATASET\n')
 tokenized_train_dataset = tokenized_datasets["train"]
@@ -64,12 +74,12 @@ print(tokenized_train_dataset.features)
 
 
 # Teste aplicando data collator
-#samples = tokenized_datasets["train"][:8]
-#samples = {k: v for k, v in samples.items() if k not in ["tweet_text"]}
-#print([len(x) for x in samples["input_ids"]])
+samples = tokenized_datasets["train"][:8]
+samples = {k: v for k, v in samples.items() if k not in ["tweet_text"]}
+print([len(x) for x in samples["input_ids"]])
 
-#batch = data_collator(samples)
-#print({k: v.shape for k, v in batch.items()})
+batch = data_collator(samples)
+print({k: v.shape for k, v in batch.items()})
 
 
 
@@ -82,34 +92,32 @@ logging_steps = len(tokenized_datasets["train"]) // batch_size
 
 
 
-# Prepara os TrainingArguments
+print('\nETAPA - TREINO\n')
 from transformers import TrainingArguments
 
-training_args = TrainingArguments(
-    output_dir=output_dir,
-    logging_dir = logging_dir,
-    overwrite_output_dir=overwrite_output_dir,
-    evaluation_strategy=evaluation_strategy,
-    learning_rate=learning_rate,
-    weight_decay=weight_decay,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    fp16=fp16,
-    logging_steps=logging_steps,
-)
+training_args = TrainingArguments("test-trainer")
 
-
-
-# Prepara o Trainer
 from transformers import Trainer
 
 trainer = Trainer(
-    model=model,
-    args=training_args,
+    model,
+    training_args,
     train_dataset=tokenized_datasets["train"],
     eval_dataset=tokenized_datasets["validation"],
     data_collator=data_collator,
+    tokenizer=tokenizer,
 )
 
-print('\nETAPA - TREINO\n')
+print(trainer.train_dataset[0])
+print(tokenizer.decode(trainer.train_dataset[0]["input_ids"]))
+print(trainer.train_dataset[0].keys())
+print(trainer.train_dataset[0]["attention_mask"])
+print(len(trainer.train_dataset[0]["attention_mask"]) == len(trainer.train_dataset[0]["input_ids"]))
+print(trainer.train_dataset[0]["labels"])
+print(trainer.train_dataset.features["labels"].names)
+
+for batch in trainer.get_train_dataloader():
+    print(batch)
+    break
+
 trainer.train()
